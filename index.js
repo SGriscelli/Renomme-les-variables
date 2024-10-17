f739be7e47e054214a2e84b9b9e7effe867109d4
SGriscelli <Simon.griscelli@wanadoo.fr>
2024-10-17 15:32:13 +0200

    Finis

---
+++ index.js
@@ -0,0 +1,13 @@
+const name = "David";
+const lastname = "Bruno";
+
+const biography =
+  "Hi, I'm David Bruno from SF, I like to cook and meet new people.";
+
+const picture = "http://www.go.com/davif.png";
+const city = "San Francisco";
+const followers = 109;
+let following = 200;
+
+following += 1;
+console.log(following);
